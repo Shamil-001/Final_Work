bf8056ac528c319d83db79b15c9097f9c4e0bc39
Shamil Magomedov <shmagomedov1997@mail.ru>
2023-08-05 21:35:14 +0300

    add diagram

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+
+
+string[] strings = { "яблоко", "банан", "кот", "пес", "слон", "лиса", "мяч" };
+string[] shortStrings = new string[strings.Length];
+int count = 0;
+
+for (int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        shortStrings[count] = strings[i];
+        count++;
+    }
+}
+
+Array.Resize(ref shortStrings, count);
+
+Console.WriteLine(string.Join(", ", strings));
+Console.WriteLine(string.Join(", ", shortStrings));
